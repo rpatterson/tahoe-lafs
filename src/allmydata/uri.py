415d414513520f595d2b925f7d404917246cfd21
Ross Patterson <me@rpatterson.net>
2020-10-08 13:39:34 -0700

    fix(py3): Cleanup redundant assignment

--- src/allmydata/uri.py
+++ src/allmydata/uri.py
@@ -19,7 +19,6 @@ if PY2:
     # Don't import bytes or str, to prevent future's newbytes leaking and
     # breaking code that only expects normal bytes.
     from future.builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, dict, list, object, range, max, min  # noqa: F401
-    str = unicode
     from builtins import int
 from past.builtins import unicode
 
